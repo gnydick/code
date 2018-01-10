12065358cf82386af4d9f5767620c2cbcc9916ad
gnydick <gnydick-github@nydick.net>
2018-01-10 14:19:37 -0800

    Create string_combinations.pl

---
+++ perl/string_combinations.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+$| = 1;
+
+my $string = shift;
+chomp $string;
+my @chars = split(//, $string);
+
+return_char("", @chars);
+
+
+
+sub return_char {
+    my ($prefix, @array) = @_;
+    for (my $i = 0; $i < @array; $i++) {
+        if (@array == 1) {
+            print $prefix . $array[$i] . "\n";
+        }
+        my @newarray = @array;
+        my $char = splice(@newarray, $i, 1);
+        return_char($prefix . $char, @newarray);
+    }
+
+}
